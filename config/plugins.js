6d7104241ba11e691a0fde7e8531b46e39ead391
moarifur <moarifur@gmail.com>
2024-06-22 16:36:01 +0600

    JWT_SECRET added to the config/plugins.js file

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,4 @@
 module.exports = () => ({
-  jwtSecret: env('JWT_SECRET'),
+  // jwtSecret: env('JWT_SECRET'),
+  jwtSecret: 'UtFwahIhANDAhHFduA0rFpVAbYRGCiV4gpy42XitOxMCD7tF+laTDhp67fXVCm4v5KLGwCoE1a97CermwZhsZw==',
 });
